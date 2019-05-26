989d4c0350f34212628de39b19f8a320e9c6f62a
Michał Chudy <chudymw@gmail.com>
2019-05-26 22:59:43 +0200

    added: component Section, partial with style for component Scection

--- js/_landing_page.jsx
+++ js/_landing_page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Hero from "./_hero.jsx";
+import Section from "./_section.jsx";
+import Contact from "./_contact.jsx";
 
 class LandingPage extends React.Component {
 
@@ -28,9 +30,65 @@ class LandingPage extends React.Component {
             },
         ];
 
+        let aboutAppContent = {
+            title: "What's it all ABOUT?",
+            items: [
+                {
+                    imgUrl: "../img/list_icon.svg",
+                    lead: "Keep a list of expenses and revenues",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                },
+                {
+                    imgUrl: "../img/visual_icon.svg",
+                    lead: "Visualize expenses and revenues",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                },
+                {
+                    imgUrl: "../img/currency_icon.svg",
+                    lead: "Accounts in foreign currencies",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                },
+            ]
+        }
+
+        let benefitsContent = {
+            title: "Why? BENEFITS.",
+            items: [
+                {
+                    imgUrl: "../img/one_place_icon.svg",
+                    lead: "Have all your finances in one place",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                },
+                {
+                    imgUrl: "../img/analyze_icon.svg",
+                    lead: "Analyze. Knowledge is power",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                }
+            ]
+        }
+
+        let aboutCreatorContent = {
+            title: "About CREATOR",
+            items: [
+                {
+                    imgUrl: "../img/me_icon.svg",
+                    lead: "Continuous improvement and running enthusiast",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                }
+            ]
+        }
+
         return(
             <div>
-                <Hero address="landing page" showSubtitle={ true } columnsParams={ columnsLandingPage } />
+                <Hero
+                    address="landing page"
+                    showSubtitle={ true }
+                    columnsParams={ columnsLandingPage }
+                />
+                <Section content={ aboutAppContent } />
+                <Section content={ benefitsContent } />
+                <Section content={ aboutCreatorContent } />
+                <Contact />
             </div>
         );
 
---
+++ js/_section.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Section extends React.Component {
+
+    render() {
+
+        return(
+            <section className="section">
+                <h2 className="section__title">
+                    { this.props.content.title }
+                </h2>
+
+                { this.props.content.items.map((el,index) => {
+
+                    return(
+                        <div
+                            className="section__item"
+                            key={ index }
+                        >
+                            <div
+                                className="section__img"
+                                style={ {backgroundImage: `url(${el.imgUrl})`} }
+                            >
+                            </div>
+                            <div className="section__text-box">
+                                <h3 className="section__lead">
+                                    { el.lead }
+                                </h3>
+                                <p className="section__text">
+                                    { el.text }
+                                </p>
+                            </div>
+                        </div>
+                    );
+                }) }
+            </section>
+        );
+    }
+}
+
+export default Section;
